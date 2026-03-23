7adabb4d90975d91d883e8a8656fe77aab653c8b
brianceco <brian.ceco@outlook.com>
2026-03-23 01:03:29 +0000

    Deploying to gh-pages from @ brianceco/brianceco.github.io@95cb5d76cd8937176726896b7b3629929799e9dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-continuous-reinforcement-learning",
+        
+          title: "(continuous) Reinforcement Learning",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/continuous-reinforcement-learning/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
