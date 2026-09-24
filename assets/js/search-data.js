f3ff5155970243ce77589d0372a9702de703fd17
brianceco <brian.ceco@outlook.com>
2026-09-24 00:56:37 +0000

    Deploying to gh-pages from @ brianceco/brianceco.github.io@acf73485d1f31990e8ebf9841e375cfb0fb0904f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
+        },{id: "nav-research",
+          title: "research",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/research/";
           },
         },{id: "post-the-profitability-of-market-making",
         
